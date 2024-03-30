61c5d84d28827c07ed5685c04eafceeed3be3ac4
Marcus Tran <144231114+MarcusTr98@users.noreply.github.com>
2024-03-30 23:38:24 +0700

    Update Lab7_bai2.cpp

--- Lab7_huyenttpp03261/Lab7_bai2.cpp
+++ Lab7_huyenttpp03261/Lab7_bai2.cpp
@@ -1,18 +1,26 @@
 #include<stdio.h>
 #include<string.h>
+#define USERNAME "admin"
+#define PASSWORD "123@abc"
 int main(){
-	char name[20];
-	char pass[20];
-	const char USERNAME[20] = "admin";
-	const char PASSWORD[20] = "123abc";	
+	//const char USERNAME[30] = "admin";
+	//const char PASSWORD[30] = "123@abc";
+	char name[30];
+	char pass[30];
 	printf("Xin moi nhap vao Username: ");
-	gets(name);
-	printf("Xin moi nhap vao PassWord: ");
-	gets(pass);	
-	if( strcmp(USERNAME, name)==0 && strcmp(PASSWORD, pass)==0 ){
+	fgets(name, sizeof(name), stdin);
+	printf("Xin moi nhap vao Password: ");
+	fgets(pass, sizeof(pass), stdin);
+	
+	name[strcspn(name, "\n")] = '\0';
+    pass[strcspn(pass, "\n")] = '\0';
+
+	// if(name[20] == username[20] && pass[20] == password[20])
+	if( strcmp(USERNAME, name) == 0 && strcmp (PASSWORD, pass) == 0 ){
 		printf("Dang nhap thanh cong!");
 	}else{
 		printf("Dang nhap that bai!");
 	}
-	return 0;
+	
+    return 0;
 }
